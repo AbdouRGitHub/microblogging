4c3aeb8ee96214606cc475212da21617c5d11f8a
Abdou <abdousani60@gmail.com>
2025-06-11 21:21:17 +0200

    feat: Enable JPA auditing in the application configuration

--- src/main/java/com/abdou/microblogging/MicrobloggingApplication.java
+++ src/main/java/com/abdou/microblogging/MicrobloggingApplication.java
@@ -2,8 +2,10 @@ package com.abdou.microblogging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MicrobloggingApplication {
 
 	public static void main(String[] args) {
